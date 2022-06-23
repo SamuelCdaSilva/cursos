a4faad5cb18181ece2b17913d457eba74ea8f76a
SamuelCdaSilva <91702874+SamuelCdaSilva@users.noreply.github.com>
2022-06-23 16:00:55 -0300

    cursos

---
+++ JavaPOO/Cursopoo/src/aula02/Caneta.java
@@ -0,0 +1,34 @@
+package aula02;
+
+
+public class Caneta {
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+    boolean tampada;
+
+    void status (){
+        System.out.println("\nModelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+
+    void rabiscar (){
+        if (this.tampada == true){
+            System.out.println("ERRO! Não é possível rabiscar!");
+        } else {
+            System.out.println("RABISCOU!");
+        }
+    }
+
+    void tampar (){
+        this.tampada = true;
+    }
+
+    void destampar (){
+        this.tampada = false;
+    }
+}
---
+++ JavaPOO/eclipse-workspace/faculdade/src/semestre/SegundoSemestre.java
@@ -0,0 +1,5 @@
+package semestre;
+
+public class SegundoSemestre {
+
+}
